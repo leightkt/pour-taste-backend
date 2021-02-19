1e72f7c550015aa4c5acb52d9927cb13a2ee1a86
Kat Leight <leightkt@gmail.com>
2021-02-19 12:02:34 -0700

    updated wine year validator message

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,7 +3,8 @@ class Wine < ApplicationRecord
     has_many :users, through: :tastings
     has_many :parties, through: :tastings
     
-    validates :brand, :wine_type, :year, presence: true
+    validates :brand, :wine_type, presence: true
     validates :name, presence: {message: "required. If name not unknown, input variety."}
     validates :variety, presence: {message: "required. If variety not unknown, input name."}
+    validates :year, presence: {message: "requred. If you don't know the wine year, use the current year."}
 end
